c996ab65a1b29215b2238f9dc6605dc3b719bfe9
Eva Simon <eva.simon@gmail.com>
2017-10-31 22:53:03 -0500

    creates Customer model and adds association

---
+++ models/customer.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+    var Customer = sequelize.define("customer", {
+        // Giving the Customer model a name of type STRING
+        customer_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // the Customer's name cannot be null
+            validate: {
+                len: [1, 50]
+            }
+        }
+    });
+
+    Customer.associate = function (models) {
+        // Associating Customers with ice_cream
+        // When a Customer is deleted, also delete any associated ice_cream
+        Customer.hasMany(models.ice_cream, {
+            onDelete: "cascade"
+        });
+    };
+
+    return Customer;
+};
--- models/ice_cream.js
+++ models/ice_cream.js
@@ -1,7 +1,32 @@
-// creates a Icecream model
+// creates an Icecream model and
+// adds validation
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define("ice_cream", {
-        ice_cream_name: DataTypes.STRING,
-        devoured: DataTypes.BOOLEAN
+    var Icecream = sequelize.define("ice_cream", {
+        ice_cream_name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            // the ice_cream's name cannot be null
+            validate: {
+                len: [1, 50]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            // an ice cream's devoured status is false by default
+            defaultValue: false
+        }
     });
+
+    Icecream.associate = function (models) {
+        // We're saying that an Ice-Cream should belong to a Customer
+        //  can't be created without an Author due to the foreign key constraint
+        Icecream.belongsTo(models.customer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Icecream;
+
 };
