a472658be362d696f34e63d99b07534cf5a385af
Eva Simon <eva.simon@gmail.com>
2017-11-01 20:10:53 -0500

    adds comments

--- controllers/ice_cream_controller.js
+++ controllers/ice_cream_controller.js
@@ -1,30 +1,25 @@
-// *********************************************************************************
 // ice_cream_controller.js - this file offers a set of routes for 
 // displaying and saving data to the db
-// *********************************************************************************
-
-// Grabbing our models
-// var db = require('../models');
-
 module.exports = function (app, db) {
+    // creates a get route
     app.get('/', function (req, res) {
-
-        db.ice_cream.findAll({ include: [db.customer] }).then(function (data) {
-            console.log(data);
+        // looks for all ice cream including the customer data and orders the ice creams by their name
+        db.ice_cream.findAll({ include: [db.customer], order: [["ice_cream_name", 'ASC']] }).then(function (data) {
+            // gets all ice creams that are not eaten yet
             var iceCream = data.filter(function (ice_cream) {
-                // console.log(ice_cream.dataValues)
                 return ice_cream.dataValues.devoured === false;
             })
-            // console.log(iceCream);
+            // gets all ice creams that are eaten
             var iceCreamEaten = data.filter(function (ice_cream) {
                 return ice_cream.dataValues.devoured === true;
             })
-            // console.log(iceCreamEaten);
+            // sends back the list of eaten and not eaten icecreams to index.handlebars where the HTML is renderend
             res.render('index', { ice_cream: iceCream, ice_cream_eaten: iceCreamEaten });
         });
     })
-
+    // creates a post route
     app.post('/', function (req, res) {
+        // test: res.status(400); res.send('None shall pass');
         db.ice_cream.create({
             ice_cream_name: req.body.ice_cream,
             devoured: false
@@ -32,15 +27,18 @@ module.exports = function (app, db) {
             res.redirect('/')
         })
     });
-
+    // creates a put route
     app.put("/:id/:customer", function (req, res) {
+        // test: res.status(400); res.send('None shall pass');
         var idValue = req.params.id;
         var customerName = req.params.customer;
 
+        // creates new user
         db.customer.create({
             customer_name: customerName
         }).then(customer => {
             var customerId = customer.id
+            // updates table for each ice cream record
             db.ice_cream.update(
                 {
                     devoured: true,
